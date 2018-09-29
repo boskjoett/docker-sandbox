8e77056972b36e8100d6f40816b099be3e60c50d
Bo Christian Skjøtt <bo@skjoett.com>
2018-09-29 08:53:30 +0200

    First commit

---
+++ CalculatorRestApi/CalculatorRestApi/Controllers/CalculatorController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Hallo from Calculator service";
+        }
+
+        // GET api/square/5
+        [HttpGet("square/{id}")]
+        public ActionResult<int> GetSquare(int id)
+        {
+            return id*id;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
