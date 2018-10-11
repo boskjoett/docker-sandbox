2f3fede2f7f253caa4b00a590a67c0dc1d1e3c3f
Bo Christian Skjøtt <bo@skjoett.com>
2018-10-11 22:00:31 +0200

    Updated

--- CalcServiceCaller/CalcServiceCaller/Database.cs
+++ CalcServiceCaller/CalcServiceCaller/Database.cs
@@ -7,10 +7,14 @@ namespace CalcServiceCaller
     {
         public void ReadMessages()
         {
-            using (var connection = new SqlConnection("Server=tcp:demodb,1433;Initial Catalog=DockerDb;Persist Security Info=True;"))
+            string connectionString = "Server=tcp:demodb,1433;Database=DockerDb;User Id=sa;Password=ZylincHello2015;";
+            Console.WriteLine("Connection string: " + connectionString);
+
+            using (var connection = new SqlConnection(connectionString))
             {
                 var command = new SqlCommand("SELECT Message1, Message2 FROM Table_2", connection);
                 connection.Open();
+
                 using (var reader = command.ExecuteReader())
                 {
                     while (reader.Read())
