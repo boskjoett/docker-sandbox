2cd2a424fbdd8995a5a822c1a2ff215a5fadbfb7
Bo Christian Skjøtt <bo@skjoett.com>
2018-10-11 20:45:21 +0200

    Updated

--- CalcServiceCaller/CalcServiceCaller/Program.cs
+++ CalcServiceCaller/CalcServiceCaller/Program.cs
@@ -1,28 +1,43 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CalcServiceCaller
 {
+    //
+    // The Docker container running the Calculator web service must be named "calcapi".
+    // No port specification is needed when two containers communicate via the bridge network.
+    //
     class Program
     {
         private static readonly HttpClient client = new HttpClient();
 
         static void Main(string[] args)
         {
-            for (int n = 2; n < 10; n++)
-            {
-                int result = GetSquare(n).Result;
-                Console.WriteLine($"The square of {n} = {result}");
-            }
+            bool repeat = true;
 
-            Console.WriteLine("Press any key to exit");
+            Console.WriteLine("Calling Calculator web service at URL http://calcapi/api/Calculator");
+
+            while (repeat)
+            {
+                for (int n = 2; n < 100; n++)
+                {
+                    int result = GetSquare(n).Result;
+                    if (result < 0)
+                        break;
+
+                    Console.WriteLine($"The square of {n} = {result}");
+
+                    Thread.Sleep(1000);
+                }
+            }
         }
 
         static async Task<int> GetSquare(int n)
         {
             int result = -1;
-            HttpResponseMessage response = await client.GetAsync($"http://localhost:5000/api/Calculator/square/{n}");
+            HttpResponseMessage response = await client.GetAsync($"http://calcapi/api/Calculator/square/{n}");
             if (response.IsSuccessStatusCode)
             {
                 string data = await response.Content.ReadAsStringAsync();
